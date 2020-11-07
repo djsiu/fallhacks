2fed42fba5f0f2475eb418909951d0eac1febfd6
djsiu <denise.j.siu@gmail.com>
2020-11-07 15:22:58 -0800

    adding a shuffle function

--- cards.cpp
+++ cards.cpp
@@ -1,5 +1,11 @@
+//cards.cpp
+
+// program creating with help from https://www.youtube.com/watch?v=iMSMVqTlvjM
+//
+
 #include <iostream>
 #include <vector>
+#include <cstdlib> //for rand
 
 using std::string;
 using std::vector;
@@ -22,6 +28,11 @@ struct Deck {
     int max_deck_size = 52;
 };
 
+void initialize(Deck&);
+void print_deck(const Deck&);
+void print_card(const Card&);
+void shuffle(Deck&);
+
 int main() {
 
     cout << "this is a deck of cards: " << endl;
@@ -29,5 +40,48 @@ int main() {
     Deck my_deck;
     initialize(my_deck);
     print_deck(my_deck);
+    cout << "\nshuffling deck..." << endl;
+    shuffle(my_deck);
+    print_deck(my_deck);
 
+}
+
+//initializing a deck of cards
+void initialize(Deck& deck) 
+{
+    Card card;
+    for (int suit = 0; suit < card.num_suits; suit++) {
+        for(int rank = 0; rank < card.num_ranks; rank++) {
+            card.suit = Suit(suit);
+            card.rank = Rank(rank);
+            deck.cards.push_back(card);
+        }
+    }
+}
+
+//prints the cards in the deck
+void print_deck(const Deck& deck) 
+{
+    for(Card c : deck.cards) {
+        print_card(c);
+    }
+}
+
+//prints a card
+void print_card(const Card& card) 
+{
+    cout << "Rank = " << card.rank << "  " 
+         << "Suit = " << card.suit << endl;
+}
+
+//shuffles the deck of cards
+void shuffle(Deck& deck) 
+{
+    Deck shuffled;
+    while(!deck.cards.empty()) {
+        size_t rand_index = rand() % deck.cards.size();
+        shuffled.cards.push_back(deck.cards[rand_index]);
+        deck.cards.erase(deck.cards.begin() + rand_index);
+    }
+    deck = shuffled;
 }

a501b8e78950f3afe644c305184daf2bbd1057ad
Jordan Kwok <jck12@sfu.ca>
2020-11-07 17:31:17 -0800

    FInished

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 #### Our idea : 
 We created a series of small games that can be played in the terminal.
 
+# First Game :
+Battleship
+1. Start by entering an x and y coordinate separated by a space to place your ship on the board.
+2. The second coordinate must be adjacent to the initial in order to complete the 2 unit long ship.
+3. After placing 2 ships, your opponent (the program) will do the same.
+4. Your goal is to "sink" both of your opponents ships by entering coordinates to attack.
+5. If you sink your opponents ships before (the program) sinks yours, you win.
+6. The battleships are at (1,1)(2,1) and (8,9)(9,9) incase you get frustrated, a rand function can be enabled to randomize the placement of the opponents ships
--- main.cpp
+++ main.cpp
@@ -7,31 +7,47 @@
 
 using namespace std;
 void user_interface(){
-    int option;
-    cout << "Which game would you like to play?" << endl;
-    cout << "1. Battleship" << endl;
-    cout << "2. Hangman" << endl;
-    cout << "3. Jelly Beans" << endl;
-    cout << "4. War" << endl;
-    cout << "5. Storytime" << endl;
-    
-    cin >> option;
-    switch(option){
-        case 1 :{
-            battleship b;
-        }case 2 :{
-            
-        }case 3 :{
-            
-        }case 4 :{
-            
-        }case 5 :{
-            
+    while(true){
+        int option;
+        cout << "Which game would you like to play?" << endl;
+        cout << "1. Battleship" << endl;
+        cout << "2. Hangman" << endl;
+        cout << "3. Jelly Beans" << endl;
+        cout << "4. War" << endl;
+        cout << "5. Storytime" << endl;
+        cout << "6. Exit" << endl;
+        
+        cin >> option;
+        switch(option){
+            case 1 :{
+                battleship b;
+                return;
+            }case 2 :{
+                
+                return;
+            }case 3 :{
+                
+                return;
+            }case 4 :{
+
+                return;    
+            }case 5 :{
+
+                return;
+            }case 6 :{
+                cout << "Thank you for playing, see you next time." << endl;
+                exit(1);
+            }default:{
+                cin.clear();
+				cin.ignore(1000,'\n');
+				cout << "Please enter the number associated with the option"<< endl;
+				break;
+            }
         }
     }
 }
 
 int main(){
-
+    user_interface();
     cout << "this is the main file" << endl;
 }

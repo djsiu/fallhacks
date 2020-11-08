4486be1e861611b4cc98725b2edcc501924888b6
Jordan Kwok <jck12@sfu.ca>
2020-11-07 16:41:54 -0800

    main with battleship

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include "Battleship.h"
 
 using namespace std;
 
 int main() {
+    battleship b;
     cout << "this is the main file" << endl;
 }

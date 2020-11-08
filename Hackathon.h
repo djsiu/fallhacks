1aa3b34c99f4a0b390d1011ea1b20a9856ee7935
Jordan Kwok <jck12@sfu.ca>
2020-11-07 16:24:02 -0800

    I'm done my game

--- Hackathon.h
+++ Hackathon.h
@@ -6,11 +6,18 @@ class battleship{
     battleship();
     ~battleship();
     void print_board();
-
+    void player_attack();
+    void print_atk_board();
+    int hitCount;
+    
     private:
+    void opponent_attack();
     void setup_player();
-    void setup_oponent();
+    void setup_opponent();
+    char opponent_board[10][10];
     char my_board[10][10];
+    char attack_board[10][10];
+    const int num_of_ships = 2;
 
 
 };
